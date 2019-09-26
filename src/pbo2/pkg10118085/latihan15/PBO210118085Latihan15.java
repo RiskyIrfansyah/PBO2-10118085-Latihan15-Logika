b3c6d42ad61004b6c2bc71d9e5a77f46a7458ef2
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:09:28 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan15/PBO210118085Latihan15.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan15;
+
+/**
+ * @author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Logika
+ * @author RISKY
+ */
+public class PBO210118085Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
